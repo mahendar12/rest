7d843034c9955a6c438914e9b349ca6bc7f7d37b
mahendar12 <mahendar.chandra@gmail.com>
2021-01-02 19:20:40 +0530

    first commit

---
+++ incomeexpensesapi/authentication/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from .views import RegisterView, VerifyEmail
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="INCOME EXPENSES API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.ourapp.com/policies/terms/",
+      contact=openapi.Contact(email="contact@expenses.local"),
+      license=openapi.License(name="Test License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+   pat(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   pat(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   pat(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   
+]
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name="register"),
+    path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
+    
+]
